b69fc5e099ec651fa8950c0a3af9c83972357623
Caperactus <102335770+Caperactus@users.noreply.github.com>
2023-08-22 18:49:00 -0500

    fixing routesv2

--- src/components/3-organisms/navBar.tsx
+++ src/components/3-organisms/navBar.tsx
@@ -7,19 +7,19 @@ export default function NavBar() {
   const { cartProducts } = useContext(ShoppingContext);
 
   const navRight = [
-    { to: '/', text: 'Shopi', customClass: 'font-semibold text-lg logo' },
-    { to: '/', text: 'All' },
-    { to: '/category/jewelery', text: 'Jewelery' },
-    { to: '/category/electronics', text: 'Electronics' },
-    { to: "/category/men's clothing", text: "men's clothing" },
-    { to: "/category/women's clothing", text: "women's clothing" },
+    { to: '/ReactWithTailwind/', text: 'Shopi', customClass: 'font-semibold text-lg logo' },
+    { to: '/ReactWithTailwind/', text: 'All' },
+    { to: '/ReactWithTailwind/category/jewelery', text: 'Jewelery' },
+    { to: '/ReactWithTailwind/category/electronics', text: 'Electronics' },
+    { to: "/ReactWithTailwind/category/men's clothing", text: "men's clothing" },
+    { to: "/ReactWithTailwind/category/women's clothing", text: "women's clothing" },
   ];
 
   const navLeft = [
     { to: undefined, text: 'caperactus@gmail.com' },
-    { to: '/my_orders', text: 'My orders' },
-    { to: '/my_account', text: 'My account' },
-    { to: '/sign_in', text: 'Sign in' },
+    { to: '/ReactWithTailwind/my_orders', text: 'My orders' },
+    { to: '/ReactWithTailwind/my_account', text: 'My account' },
+    { to: '/ReactWithTailwind/sign_in', text: 'Sign in' },
   ];
   return (
     <nav className="bg-white flex justify-between items-center fixed top-0 z-10 w-full py-5 px-8 text-sm font-light">
